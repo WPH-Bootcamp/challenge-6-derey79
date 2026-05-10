65783646ce0e51486f0c4fd9c97adcebdd30915f
derey79 <devyreymond@gmail.com>
2026-05-10 13:40:21 +0700

    import function addBook, listBook and searchBook, update execute program

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
 export {};
-import { showMenu, mainMenu } from './functions/bookManager';
-import { mainmenuList } from './data/books';
+import { mainmenuList, newBooks, newBooksInd } from './data/books';
+import {
+  showMenu,
+  addBooks,
+  listBooks,
+  searchBook,
+} from './functions/bookManager';
 
 // File ini adalah entry point aplikasi
 // Gunakan file ini untuk menguji implementasi yang sudah dibuat
@@ -16,5 +21,20 @@ console.log('=====================================');
 
 // Mulai pengujian di bawah ini
 
+// Fungsi untuk menjalankan langkah berdasarkan async
+const runBookApps = async () => {
+  console.log('Step [1]. Add New Book to Array');
+  await addBooks(newBooks);
+  await addBooks(newBooksInd);
+  console.log('\nStep [2]. List of Book from Array');
+  listBooks();
+  console.log('Step [3]. Search Book in Array');
+  searchBook('PAULO');
+  searchBook(2017);
+  searchBook('Cerita');
+};
+
+// menampilkan Menu
 showMenu(mainmenuList);
-mainMenu();
+// eksekusi porgram
+runBookApps();
